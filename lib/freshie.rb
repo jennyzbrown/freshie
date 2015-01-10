eea7362f0e6d4a9bf0b7b3297b3a14ce3c252d55
Jenny Brown <jennyzbrown@gmail.com>
2015-01-10 12:59:37 -0500

    Removed class/end

--- lib/freshie.rb
+++ lib/freshie.rb
@@ -1,10 +1,8 @@
 require "freshie/version"
 
 module Freshie
-  class Freshie
     def self.fresh(array)
       random_index = Random.rand(array.length)
       array[random_index]
-    end
   end
 end
